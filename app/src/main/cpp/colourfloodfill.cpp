be4e281b79838e5b2a4d22c27a282d688107d0a0
Lakmal Weerasekara <alrweerasekara@gmail.com>
2021-05-09 16:58:09 +0530

    updated flood fill algorith

--- app/src/main/cpp/colourfloodfill.cpp
+++ app/src/main/cpp/colourfloodfill.cpp
@@ -6,30 +6,40 @@
 //
 
 extern "C" {
-/*JNIEXPORT jstring JNICALL Java_com_noah_floodfill_activity_MainActivity_getTestString(JNIEnv *env);*/
-
-JNIEXPORT void JNICALL Java_com_noah_floodfill_activity_MainActivity_floodFill(JNIEnv *env, jobject obj, jintArray pixels, jint w,
-                                                                               jint h, jobject pt, jint targetColor,
-                                                                               jint fillColor);
+JNIEXPORT void JNICALL Java_com_lakmalz_floodfill_helpers_ColourFill_floodFill(JNIEnv *env,
+                                                                                              jobject obj,
+                                                                                              jobject bitmap,
+                                                                                              uint32_t x,
+                                                                                              uint32_t y,
+                                                                                              uint32_t fillColor,
+                                                                                              uint32_t targetColor,
+                                                                                              uint32_t tolerance);
 
 }
 
 
-JNIEXPORT jobject JNICALL Java_com_golysoft_view_DrawThread_floodFillScanLineQueue(JNIEnv *env, jobject obj, jintArray pixels,
-                                                                                   jint w,
-                                                                                   jint h, jobject pt, jint targetColor,
-                                                                                   jint fillColor) {
+JNIEXPORT void JNICALL Java_com_lakmalz_floodfill_helpers_ColourFill_floodFill(JNIEnv *env,
+                                                                                              jobject obj,
+                                                                                              jobject bitmap,
+                                                                                              uint32_t x,
+                                                                                              uint32_t y,
+                                                                                              uint32_t fillColor,
+                                                                                              uint32_t targetColor,
+                                                                                              uint32_t tolerance) {
+    AndroidBitmapInfo bitmapInfo;
 
+    if ((AndroidBitmap_getInfo(env, bitmap, &bitmapInfo)) < 0) {
+        return;
+    }
 
-    if(targetColor == fillColor) return NULL;
+    if (bitmapInfo.format != ANDROID_BITMAP_FORMAT_RGBA_8888) {
+        return;
+    }
+    void *bitmapPixels;
+    if ((AndroidBitmap_lockPixels(env, bitmap, &bitmapPixels)) < 0) {
+        return;
+    }
 
-    jint *pixelPtr;
-
-    jboolean spanLeft = JNI_FALSE, spanRight = JNI_FALSE;
-    jboolean fullScreen = JNI_TRUE;
+    floodFill(env, x, y, fillColor, bitmapPixels, &bitmapInfo, tolerance, false);
+    AndroidBitmap_unlockPixels(env, bitmap);
 }
-
-
-/*JNIEXPORT jstring JNICALL Java_com_noah_floodfill_activity_MainActivity_getTestString(JNIEnv *env) {
-    return (*env).NewStringUTF("Clicked.");
-}*/
